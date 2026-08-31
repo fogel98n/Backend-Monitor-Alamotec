8ec6cfbf90416fbbb4c21d0aa4dfdd70e2b48cfa
Fernando Orellana Barrios <fogel98nex@gmail.com>
2026-08-31 11:57:52 -0600

    .

--- Backend-MonitorAlmotec/src/index.js
+++ Backend-MonitorAlmotec/src/index.js
@@ -5,13 +5,13 @@ const app= express()
 const PORT=3000;
 
 app.use(cors({
-    origin:['https://monitor.alamotec.com.gt'],methods:["GET","POST","PUT","DELETE"],allowedHeaders:["Content-Type"]
+    origin:['https://monitor.alamotec.com.gt','http://localhost:5173'],methods:["GET","POST","PUT","DELETE"],allowedHeaders:["Content-Type"]
 }))
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.use("/",routes)
+app.use("/api",routes)
 
 app.listen(PORT,()=>{
     console.log(`servidor correindo en el puerto ${PORT}`)
